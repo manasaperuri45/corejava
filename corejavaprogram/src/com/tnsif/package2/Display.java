6846c5b08f6a1776a584c002335c27a8dc5cac65
manasaperuri45 <manasaperuri175@gmail.com>
2024-07-03 14:06:32 +0530

    commit message

---
+++ corejavaprogram/src/com/tnsif/package2/Display.java
@@ -0,0 +1,11 @@
+package com.tnsif.package2;
+import com.tnsif.packages.*; 
+public class Display {
+	public static void main(String[] args) {
+		Publicdemo p= new Publicdemo();
+		System.out.println(p.a);
+		
+		
+	}
+
+}
---
+++ corejavaprogram/src/com/tnsif/scannerclass/Scannerexample.java
@@ -0,0 +1,16 @@
+package com.tnsif.scannerclass;
+
+import java.util.Scanner;
+
+//scanner demo
+public class Scannerexample {
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("enter your name");
+	    String name=sc.nextLine();
+	    System.out.println("enter your roll number");
+	    int rollno=sc.nextInt();
+	
+	}
+
+}
