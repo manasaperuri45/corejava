b6deb52ca0f73c0e418a9aec6ecef87f3b6c0b2c
manasaperuri45 <manasaperuri175@gmail.com>
2024-07-03 15:51:06 +0530

    commit

---
+++ corejavaprogram/src/com/tnsif/encapsulation/Encapsulationdemo.java
@@ -0,0 +1,10 @@
+package com.tnsif.encapsulation;
+
+public class Encapsulationdemo {
+int serialnumber;
+String name;
+int age;
+	void show() {
+      System.out.println("serial number"+serialnumber+"name"+name+"age"+age); 	
+}
+}
